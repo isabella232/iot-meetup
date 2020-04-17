10a986662ad72d7a6a20b5e45cebdc004c5a2c37
Tom de Brouwer <tlj.debrouwer@gmail.com>
2020-04-17 13:55:49 +0200

    Add run scripts

---
+++ Docker/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+docker load < output/iot-hands-on-1.0.0.docker.tar
+
+docker run -v "<directory to certificate>":/tmp \
+           -e CERTIFCATE=<certificate file name e.g. 1234-certificate.pem.crt> \
+           -e PRIVATE_KEY=<private key file name e.g. 1234-private.pem.key> \
+           -e THING_NAME=<your thing name> \
+           -e IOT_ENDPOINT=a3g5girgh7pd7z-ats.iot.eu-west-1.amazonaws.com \
+           -i \
+           -t iot-hands-on:1.0.0
